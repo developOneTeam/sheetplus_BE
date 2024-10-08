e691df2a5f18c47a0bdfb1c9a1516a99bc541742
hwangjeyeon <kingpele9018@gmail.com>
2024-10-09 01:11:14 +0900

    Feat: Jwt 토큰을 주고받을 DTO 구현

---
+++ checking/src/main/java/sheetplus/checking/domain/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package sheetplus.checking.domain.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@Builder @AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String accessToken;
+    private String refreshToken;
+    private MemberInfoDto memberInfo;
+
+}
