9e9de8265479378283d71605fefa1e1c39ffff2e
hwangjeyeon <kingpele9018@gmail.com>
2024-10-11 17:52:49 +0900

    Fix: 삭제된 도메인에 영향 받는 도메인코드 수정

--- checking/src/main/java/sheetplus/checking/domain/entity/Prize.java
+++ checking/src/main/java/sheetplus/checking/domain/entity/Prize.java
@@ -23,20 +23,9 @@ public class Prize {
     @Column(nullable = false)
     private ReceiveCondition receiveCondition;
 
-    @OneToOne(fetch = FetchType.LAZY, orphanRemoval = true)
-    @JoinColumn(name = "participate_state_id")
-    private ParticipateState participateStatePrize;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "contest_id")
     private Contest contestPrize;
-
-    public void setParticipateStatePrize(ParticipateState participateState) {
-        this.participateStatePrize = participateState;
-        if(participateStatePrize.getPrizeParticipateState() != this){
-            participateStatePrize.setPrizeParticipateState(this);
-        }
-    }
 
     public void setContestPrize(Contest contestPrize) {
         this.contestPrize = contestPrize;
