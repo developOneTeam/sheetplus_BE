cdd1984f65934951945d57edfda5077811c3ce4f
hwangjeyeon <kingpele9018@gmail.com>
2024-10-11 20:42:41 +0900

    Feat: Entry CUD 서비스 기능 구현

---
+++ checking/src/main/java/sheetplus/checking/domain/service/EntryCRUDService.java
@@ -0,0 +1,69 @@
+package sheetplus.checking.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sheetplus.checking.domain.dto.EntryRequestDto;
+import sheetplus.checking.domain.dto.EntryResponseDto;
+import sheetplus.checking.domain.entity.Entry;
+import sheetplus.checking.domain.repository.EntryRepository;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class EntryCRUDService {
+
+    private final EntryRepository entryRepository;
+
+
+    @Transactional
+    public EntryResponseDto createEntry(EntryRequestDto entryRequestDto){
+        Entry entry = Entry.builder()
+                .name(entryRequestDto.getName())
+                .location(entryRequestDto.getLocation())
+                .building(entryRequestDto.getBuilding())
+                .teamNumber(entryRequestDto.getTeamNumber())
+                .professorName(entryRequestDto.getProfessorName())
+                .leaderName(entryRequestDto.getLeaderName())
+                .entryType(entryRequestDto.getEntryType())
+                .build();
+        Long id = entryRepository.save(entry).getId();
+
+        return EntryResponseDto.builder()
+                .id(id)
+                .name(entry.getName())
+                .location(entry.getLocation())
+                .building(entry.getBuilding())
+                .teamNumber(entry.getTeamNumber())
+                .professorName(entry.getProfessorName())
+                .leaderName(entry.getLeaderName())
+                .entryType(entry.getEntryType().getMessage())
+                .build();
+    }
+
+    @Transactional
+    public EntryResponseDto updateEntry(Long id, EntryRequestDto entryRequestDto){
+        Entry entry = entryRepository.findById(id).orElse(null);
+        entry.updateEntry(entryRequestDto);
+
+
+        return EntryResponseDto.builder()
+                .id(id)
+                .name(entry.getName())
+                .location(entry.getLocation())
+                .building(entry.getBuilding())
+                .teamNumber(entry.getTeamNumber())
+                .professorName(entry.getProfessorName())
+                .leaderName(entry.getLeaderName())
+                .entryType(entry.getEntryType().getMessage())
+                .build();
+    }
+
+    @Transactional
+    public void deleteEntry(Long id){
+        entryRepository.deleteById(id);
+    }
+
+
+}
