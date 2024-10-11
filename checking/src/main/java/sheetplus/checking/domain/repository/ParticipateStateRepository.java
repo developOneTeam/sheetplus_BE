366644d29eeb654f328e0087bdb325ddb9d806af
hwangjeyeon <kingpele9018@gmail.com>
2024-10-11 13:41:40 +0900

    Delete: 컬렉션 필드 도입으로 해당 도메인 삭제

--- checking/src/main/java/sheetplus/checking/domain/repository/ParticipateStateRepository.java
+++
@@ -1,7 +0,0 @@
-package sheetplus.checking.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sheetplus.checking.domain.entity.ParticipateState;
-
-public interface ParticipateStateRepository extends JpaRepository<ParticipateState, Long> {
-}
