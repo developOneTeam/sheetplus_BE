81feb88d4a5ca7c0336f34c79c3e70bf312d1c84
hwangjeyeon <kingpele9018@gmail.com>
2024-10-11 13:40:42 +0900

    Feat: 참가 이벤트들을 관리하기 위한 Set 컬렉션 타입 필드 구현

--- checking/src/main/java/sheetplus/checking/domain/entity/ParticipateContestState.java
+++ checking/src/main/java/sheetplus/checking/domain/entity/ParticipateContestState.java
@@ -5,8 +5,11 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import sheetplus.checking.domain.entity.enums.EventType;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -37,6 +40,14 @@ public class ParticipateContestState {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "contest_id")
     private Contest contestParticipateContestState;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "participate_event_type",
+            joinColumns = @JoinColumn(name = "participate_contest_state_id")
+    )
+    @Builder.Default
+    private Set<EventType> eventTypeSet = new HashSet<>();
 
 
     public void setMemberParticipateContestStates(Member member){
