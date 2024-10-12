4342fbe44454d6e3ab3760effde9eaa9a6365c95
hwangjeyeon <kingpele9018@gmail.com>
2024-10-12 21:12:16 +0900

    Feat: 로그인 페이지 내 잘못된 로직 삭제

--- checking/src/main/java/sheetplus/checking/domain/controller/MemberController.java
+++ checking/src/main/java/sheetplus/checking/domain/controller/MemberController.java
@@ -3,13 +3,14 @@ package sheetplus.checking.domain.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sheetplus.checking.domain.dto.*;
+import sheetplus.checking.domain.dto.LoginDto;
+import sheetplus.checking.domain.dto.MemberRequestDto;
+import sheetplus.checking.domain.dto.MemberUpdateRequestDto;
+import sheetplus.checking.domain.dto.TokenDto;
 import sheetplus.checking.domain.entity.Member;
 import sheetplus.checking.domain.service.AuthService;
 import sheetplus.checking.domain.service.CommonPageService;
 import sheetplus.checking.domain.service.MemberCRUDService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,10 +18,9 @@ public class MemberController {
 
     private final MemberCRUDService memberCRUDService;
     private final AuthService authService;
-    private final CommonPageService commonPageService;
 
     @PostMapping("public/register")
-    public ResponseEntity<MemberCreateResponseDto> createMember(
+    public ResponseEntity<TokenDto> createMember(
             @RequestBody MemberRequestDto memberRequestDto){
         Member member = memberCRUDService.createMember(memberRequestDto);
         TokenDto tokenWithData = authService.memberLogin(LoginDto.builder()
@@ -28,12 +28,9 @@ public class MemberController {
                         .email(member.getUniversityEmail())
                         .memberType(member.getMemberType())
                 .build());
-        List<ContestInfoResponseDto> contestNames = commonPageService.readContestInfo();
+
         return ResponseEntity.ok()
-                .body(MemberCreateResponseDto.builder()
-                        .tokenDto(tokenWithData)
-                        .contestInfoResponseDto(contestNames)
-                        .build());
+                .body(tokenWithData);
     }
 
 
