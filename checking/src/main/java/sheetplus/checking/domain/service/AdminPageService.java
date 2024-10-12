d68e5061d908814ad71df84a63b98a292cd19aab
hwangjeyeon <kingpele9018@gmail.com>
2024-10-12 21:07:20 +0900

    Feat: Admin 페이지 Service 구현

---
+++ checking/src/main/java/sheetplus/checking/domain/service/AdminPageService.java
@@ -0,0 +1,120 @@
+package sheetplus.checking.domain.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sheetplus.checking.domain.dto.*;
+import sheetplus.checking.domain.entity.Contest;
+import sheetplus.checking.domain.entity.Event;
+import sheetplus.checking.domain.repository.ContestRepository;
+import sheetplus.checking.domain.repository.EntryRepository;
+import sheetplus.checking.domain.repository.MemberRepository;
+import sheetplus.checking.domain.repository.ParticipateContestStateRepository;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AdminPageService {
+
+    private final ContestRepository contestRepository;
+    private final MemberRepository memberRepository;
+    private final ParticipateContestStateRepository participateContestStateRepository;
+    private final EntryRepository entryRepository;
+
+    @Transactional(readOnly = true)
+    public AdminHomeResponseDto adminHomeRead(Long contestId){
+        Contest contest = contestRepository
+                .findById(contestId).orElse(null);
+        Long memberCounts = memberRepository.count();
+        ParticipateInfoDto participateInfoDto = participateContestStateRepository
+                .participateContestCounts(contest.getId());
+        List<Event> events = contest.getEvents();
+        long remain = 0;
+        long finish = 0;
+        TreeSet<String> building = new TreeSet<>();
+        HashSet<String> major = new HashSet<>();
+        events.sort((o1, o2) -> {
+            if(o1.getStartTime().equals(o2.getStartTime())){
+                return o1.getEndTime().compareTo(o2.getEndTime());
+            }
+            return o1.getStartTime().compareTo(o2.getStartTime());
+        });
+
+        for (int i = 0; i < events.size(); i++) {
+            building.add(events.get(i).getBuilding());
+            major.add(events.get(i).getMajor());
+            LocalDateTime nowTime = LocalDateTime.now();
+
+            if(events.get(i).getStartTime().getDayOfMonth()
+                    == nowTime.getDayOfMonth() &&
+                    (events.get(i).getStartTime().isAfter(nowTime)
+                            || events.get(i).getEndTime().isAfter(nowTime))){
+                remain++;
+            }else{
+                finish++;
+            }
+        }
+        EntryInfoDto entryInfoDto = entryRepository.entryInfoCounts();
+        List<EntryResponseDto> entryList = entryRepository.getEntry();
+
+
+        return AdminHomeResponseDto.builder()
+                .memberCounts(memberCounts.toString())
+                .completeEventMemberCounts(participateInfoDto
+                        .getCompleteEventMemberCounts().toString())
+                .moreThanOneCounts(participateInfoDto
+                        .getMoreThanOneCounts().toString())
+                .moreThanFiveCounts(participateInfoDto
+                        .getMoreThanFiveCounts().toString())
+                .contestName(contest.getName())
+                .contestStart(contest.getStartDate())
+                .contestEnd(contest.getEndDate())
+                .locationName(building.getFirst())
+                .locationCounts(String.valueOf(building.size()))
+                .remainEvents(String.valueOf(remain))
+                .finishEvents(String.valueOf(finish))
+                .notTodayEvents(String.valueOf((events.size() - (remain + finish))))
+                .entryMajorCounts(entryInfoDto.getMajorCounts().toString())
+                .entryCounts(entryInfoDto.getTotalCounts().toString())
+                .entryPreliminaryCounts(entryInfoDto.getPreliminaryCounts().toString())
+                .entryFinalCounts(entryInfoDto.getFinalCounts().toString())
+                .eventCounts(String.valueOf(events.size()))
+                .allEvents(events.stream()
+                        .map(p -> EventResponseDto.builder()
+                                .secureId(p.getId().toString())
+                                .name(p.getName())
+                                .startTime(p.getStartTime())
+                                .endTime(p.getEndTime())
+                                .location(p.getLocation())
+                                .building(p.getBuilding())
+                                .speakerName(p.getSpeakerName())
+                                .major(p.getMajor())
+                                .conditionMessage(p.getEventCondition().getMessage())
+                                .eventTypeMessage(p.getEventType().getMessage())
+                                .categoryMessage(p.getEventCategory().getMessage())
+                                .build())
+                        .toList())
+                .entryPageable(entryList)
+                .build();
+    }
+
+
+    public List<MemberInfoDto> readDrawMemberList(Long contestId){
+        Contest contest = contestRepository.findById(contestId).orElse(null);
+        return participateContestStateRepository
+                .participateMemberInfoRead(contest.getId());
+    }
+
+    public List<MemberInfoDto> readPrizeMemberList(Long contestId){
+        Contest contest = contestRepository.findById(contestId).orElse(null);
+        return participateContestStateRepository
+                .drawMemberInfoRead(contest.getId());
+    }
+
+
+}
