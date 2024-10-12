bfa3794867c11313cd63aee7c3c0eecb66eb735a
hwangjeyeon <kingpele9018@gmail.com>
2024-10-12 21:07:11 +0900

    Feat: Admin 페이지 dto 구현

---
+++ checking/src/main/java/sheetplus/checking/domain/dto/AdminHomeRequestDto.java
@@ -0,0 +1,15 @@
+package sheetplus.checking.domain.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AdminHomeRequestDto {
+
+    private Long contestId;
+
+}
