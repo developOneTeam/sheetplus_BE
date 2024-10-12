c2ddd17977a0251751350288df0fa629307e32f7
hwangjeyeon <kingpele9018@gmail.com>
2024-10-12 21:05:46 +0900

    Feat: Querydsl을 위한 Custom Repository 구현
    - ParticipateContestState 도메인
    - 추첨대상/증정대상/대상인원 숫자 반환하는 기능 구현

--- checking/src/main/java/sheetplus/checking/domain/repository/ParticipateContestStateRepositoryCustomImpl.java
+++ checking/src/main/java/sheetplus/checking/domain/repository/ParticipateContestStateRepositoryCustomImpl.java
@@ -3,7 +3,12 @@ package sheetplus.checking.domain.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import sheetplus.checking.domain.dto.MemberInfoDto;
+import sheetplus.checking.domain.dto.ParticipateInfoDto;
+import sheetplus.checking.domain.entity.Member;
 import sheetplus.checking.domain.entity.enums.MeritType;
+
+import java.util.List;
 
 import static sheetplus.checking.domain.entity.QParticipateContestState.participateContestState;
 
@@ -20,5 +25,69 @@ public class ParticipateContestStateRepositoryCustomImpl implements ParticipateC
                 .set(participateContestState.meritType, MeritType.TARGET)
                 .where(participateContestState.eventsCount.goe(condition))
                 .execute();
+    }
+
+    @Override
+    public ParticipateInfoDto participateContestCounts(Long id) {
+        Long countOne = queryFactory
+                .select(participateContestState.count())
+                .from(participateContestState)
+                .where(participateContestState.eventsCount.goe(1))
+                .fetchOne();
+        Long countAll = queryFactory
+                .select(participateContestState.count())
+                .from(participateContestState)
+                .where(participateContestState.eventsCount.goe(5))
+                .fetchOne();
+
+        return ParticipateInfoDto.builder()
+                .completeEventMemberCounts(countAll)
+                .moreThanOneCounts(countOne)
+                .moreThanFiveCounts(countAll)
+                .build();
+    }
+
+    @Override
+    public List<MemberInfoDto> participateMemberInfoRead(Long contestId) {
+        List<Member> members = queryFactory
+                .select(participateContestState.memberParticipateContestState)
+                .from(participateContestState)
+                .where(participateContestState.contestParticipateContestState.id.eq(contestId).and(
+                        participateContestState.eventsCount.goe(5).or(
+                                participateContestState.meritType.eq(MeritType.TARGET)
+                        )
+                ))
+                .fetch();
+
+        return members.stream()
+                .map(p -> MemberInfoDto.builder()
+                        .id(p.getId())
+                        .name(p.getName())
+                        .email(p.getUniversityEmail())
+                        .major(p.getMajor())
+                        .memberType(p.getMemberType())
+                        .studentId(p.getStudentId())
+                        .build())
+                .toList();
+    }
+
+    @Override
+    public List<MemberInfoDto> drawMemberInfoRead(Long contestId) {
+        List<Member> members = queryFactory
+                .select(participateContestState.memberParticipateContestState)
+                .from(participateContestState)
+                .where(participateContestState.contestParticipateContestState.id.eq(contestId))
+                .fetch();
+
+        return members.stream()
+                .map(p -> MemberInfoDto.builder()
+                        .id(p.getId())
+                        .name(p.getName())
+                        .email(p.getUniversityEmail())
+                        .major(p.getMajor())
+                        .memberType(p.getMemberType())
+                        .studentId(p.getStudentId())
+                        .build())
+                .toList();
     }
 }
