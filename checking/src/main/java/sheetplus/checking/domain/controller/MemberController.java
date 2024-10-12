f43daa8a27fd3d23de33efd1489735435c85c492
hwangjeyeon <kingpele9018@gmail.com>
2024-10-12 21:03:09 +0900

    Feat: 공통 기능 적용을 위한 컨트롤러 내 DTO 변경

--- checking/src/main/java/sheetplus/checking/domain/controller/MemberController.java
+++ checking/src/main/java/sheetplus/checking/domain/controller/MemberController.java
@@ -3,13 +3,13 @@ package sheetplus.checking.domain.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sheetplus.checking.domain.dto.LoginDto;
-import sheetplus.checking.domain.dto.MemberRequestDto;
-import sheetplus.checking.domain.dto.MemberUpdateRequestDto;
-import sheetplus.checking.domain.dto.TokenDto;
+import sheetplus.checking.domain.dto.*;
 import sheetplus.checking.domain.entity.Member;
 import sheetplus.checking.domain.service.AuthService;
+import sheetplus.checking.domain.service.CommonPageService;
 import sheetplus.checking.domain.service.MemberCRUDService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +17,10 @@ public class MemberController {
 
     private final MemberCRUDService memberCRUDService;
     private final AuthService authService;
+    private final CommonPageService commonPageService;
 
     @PostMapping("public/register")
-    public ResponseEntity<TokenDto> createMember(
+    public ResponseEntity<MemberCreateResponseDto> createMember(
             @RequestBody MemberRequestDto memberRequestDto){
         Member member = memberCRUDService.createMember(memberRequestDto);
         TokenDto tokenWithData = authService.memberLogin(LoginDto.builder()
@@ -27,9 +28,12 @@ public class MemberController {
                         .email(member.getUniversityEmail())
                         .memberType(member.getMemberType())
                 .build());
-
+        List<ContestInfoResponseDto> contestNames = commonPageService.readContestInfo();
         return ResponseEntity.ok()
-                .body(tokenWithData);
+                .body(MemberCreateResponseDto.builder()
+                        .tokenDto(tokenWithData)
+                        .contestInfoResponseDto(contestNames)
+                        .build());
     }
 
 
