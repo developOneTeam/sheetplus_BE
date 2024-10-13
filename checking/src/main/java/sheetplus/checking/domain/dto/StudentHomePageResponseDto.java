0bb86932ae5b605c9b5e35f2f18e14301e3bcfdf
hwangjeyeon <kingpele9018@gmail.com>
2024-10-13 20:55:10 +0900

    Feat: StudentPage Read dto 구현

---
+++ checking/src/main/java/sheetplus/checking/domain/dto/StudentHomePageResponseDto.java
@@ -0,0 +1,20 @@
+package sheetplus.checking.domain.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StudentHomePageResponseDto {
+
+    private String studentName;
+    private String studentMajor;
+
+    private String eventCounts;
+    private List<EventResponseDto> events;
+
+}
