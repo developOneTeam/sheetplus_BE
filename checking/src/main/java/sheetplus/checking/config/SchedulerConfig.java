49229fd89c8bc73328aec048b4d7e0155c2396b9
hwangjeyeon <kingpele9018@gmail.com>
2024-10-15 01:03:35 +0900

    Config: 스케줄러 설정 추가

---
+++ checking/src/main/java/sheetplus/checking/config/SchedulerConfig.java
@@ -0,0 +1,20 @@
+package sheetplus.checking.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.TaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("Event-Start-notification-scheduler");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
