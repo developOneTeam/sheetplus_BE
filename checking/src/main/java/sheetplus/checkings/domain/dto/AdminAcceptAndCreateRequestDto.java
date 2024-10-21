4f535e0c28bdab0ac58be8446d760fa8678568b6
hwangjeyeon <kingpele9018@gmail.com>
2024-10-22 01:14:22 +0900

    Feat: ADMIN 계정 승인상태 관리 DTO 개발

---
+++ checking/src/main/java/sheetplus/checkings/domain/dto/AdminAcceptAndCreateRequestDto.java
@@ -0,0 +1,14 @@
+package sheetplus.checkings.domain.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AdminAcceptAndCreateRequestDto {
+
+    private String email;
+
+}
