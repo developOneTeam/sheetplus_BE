fccad67e211b65c20ee696b30f8f900d80f810b9
hwangjeyeon <kingpele9018@gmail.com>
2024-10-22 22:26:02 +0900

    Feat: 멤버 로그인 서비스 기능 개발
    - 기존 로그인 메소드에서 loginDto의 id값 null 여부에 따라 구분
    - null이면 email로, 아니면 id로 검색 후, 로그인 진행
    - id가 null일 경우 토큰 발행 시, 예외발생하므로 dto에 set 설정 구현

--- checking/src/main/java/sheetplus/checkings/domain/service/AuthService.java
+++ checking/src/main/java/sheetplus/checkings/domain/service/AuthService.java
@@ -11,8 +11,6 @@ import sheetplus.checkings.domain.dto.TokenDto;
 import sheetplus.checkings.domain.entity.Member;
 import sheetplus.checkings.domain.entity.enums.MemberType;
 import sheetplus.checkings.domain.repository.MemberRepository;
-import sheetplus.checkings.domain.repository.TemporaryMemberRepository;
-import sheetplus.checkings.error.ErrorCodeIfs;
 import sheetplus.checkings.exception.ApiException;
 import sheetplus.checkings.util.JwtUtil;
 
@@ -26,31 +24,26 @@ public class AuthService {
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
     private final TokenService refreshTokenService;
-    private final TemporaryMemberRepository temporaryMemberRepository;
 
     // 최초 토큰 생성에 대해서 발급
     @Transactional
     public TokenDto memberLogin(LoginDto loginDto){
-        Member member = memberRepository.findById(loginDto.getId())
+        Member member = loginDto.getId() != null ? memberRepository.findById(loginDto.getId())
+                .orElseThrow(() -> new ApiException(MEMBER_NOT_FOUND))
+                : memberRepository.findMemberByUniversityEmail(loginDto.getEmail())
                 .orElseThrow(() -> new ApiException(MEMBER_NOT_FOUND));
 
+        if(!member.getMemberType().equals(loginDto.getMemberType())){
+            log.info("타입 불일치 멤버 요청");
+            throw new ApiException(MEMBER_TYPE_DISCREPANCY);
+        }
 
         if(!member.getUniversityEmail().equals(loginDto.getEmail())){
             log.info("이메일이 일치하지 않습니다. 토큰 변조 위험이 있습니다.");
-            throw new ApiException(new ErrorCodeIfs() {
-                @Override
-                public Integer getHttpStatusCode() {
-                    return 403;
-                }
-
-                @Override
-                public String getErrorDescription() {
-                    return "이메일이 일치하지 않습니다. 토큰 변조 위험이 있습니다.";
-                }
-            });
+            throw new ApiException(MEMBER_EMAIL_DISCREPANCY);
         }
 
-
+        loginDto.setId(member.getId());
         String accessToken = jwtUtil.createAccessToken(loginDto);
         String refreshToken = jwtUtil.createRefreshToken(loginDto);
 
@@ -79,8 +72,7 @@ public class AuthService {
     @Transactional
     public boolean memberTypeCheck(MemberType memberType){
         if(memberType.equals(MemberType.SUPER_ADMIN)){
-            // 로그인 기능 개발 전까지는 허용
-            //throw new ApiException(SUPER_ADMIN_REGISTER_BLOCK);
+            throw new ApiException(SUPER_ADMIN_REGISTER_BLOCK);
         }
 
         if(memberType.equals(MemberType.ADMIN) ||
