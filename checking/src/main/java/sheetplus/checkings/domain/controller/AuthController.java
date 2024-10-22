5a338d2677e41ca490385e9bf8b6fa50846c87eb
hwangjeyeon <kingpele9018@gmail.com>
2024-10-22 22:26:34 +0900

    Feat: 멤버 로그인 컨트롤러 기능 개발

--- checking/src/main/java/sheetplus/checkings/domain/controller/AuthController.java
+++ checking/src/main/java/sheetplus/checkings/domain/controller/AuthController.java
@@ -3,8 +3,11 @@ package sheetplus.checkings.domain.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import sheetplus.checkings.domain.dto.LoginDto;
+import sheetplus.checkings.domain.dto.MemberLoginRequestDto;
 import sheetplus.checkings.domain.dto.TokenDto;
 import sheetplus.checkings.domain.service.AuthService;
 import sheetplus.checkings.response.Api;
@@ -14,10 +17,23 @@ import sheetplus.checkings.response.Api;
 @Slf4j
 public class AuthController {
     public final AuthService authService;
+
     @PostMapping("public/refresh")
     public Api<TokenDto> refreshToken(@RequestHeader(value = "refresh-token"
             , required = false) String refreshToken){
         TokenDto tokenDto = authService.refreshTokens(refreshToken);
+        return Api.OK(tokenDto);
+    }
+
+    @PostMapping("public/login")
+    public Api<TokenDto> loginMember(
+            @RequestBody MemberLoginRequestDto memberLoginRequestDto){
+
+        TokenDto tokenDto = authService.memberLogin(LoginDto.builder()
+                        .id(null)
+                        .email(memberLoginRequestDto.getEmail())
+                        .memberType(memberLoginRequestDto.getMemberType())
+                .build());
         return Api.OK(tokenDto);
     }
 
