6391d66e70209396db22edd0512a8cfe8817af74
hwangjeyeon <kingpele9018@gmail.com>
2024-10-22 22:23:24 +0900

    Feat: 예외처리를 편하게 하기 위해 반환타입 변경

--- checking/src/main/java/sheetplus/checkings/domain/repository/MemberRepository.java
+++ checking/src/main/java/sheetplus/checkings/domain/repository/MemberRepository.java
@@ -3,9 +3,9 @@ package sheetplus.checkings.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sheetplus.checkings.domain.entity.Member;
 
-import java.util.List;
+import java.util.Optional;
 
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findMemberByUniversityEmail(String email);
+    Optional<Member> findMemberByUniversityEmail(String email);
 }
