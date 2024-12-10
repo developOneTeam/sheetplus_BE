fe7fb742e472e05387e78b4e7415d61085b22050
hwangjeyeon <kingpele9018@gmail.com>
2024-12-11 00:25:38 +0900

    Config: FCM 최대 스레드 생성 제한을 위한 커스텀 스레드 매니저 구현

---
+++ src/main/java/sheetplus/checkings/config/manager/CustomThreadManager.java
@@ -0,0 +1,25 @@
+package sheetplus.checkings.config.manager;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ThreadManager;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class CustomThreadManager extends ThreadManager {
+    @Override
+    protected ExecutorService getExecutor(FirebaseApp firebaseApp) {
+        return Executors.newFixedThreadPool(100);
+    }
+
+    @Override
+    protected void releaseExecutor(FirebaseApp firebaseApp, ExecutorService executorService) {
+        executorService.shutdown();
+    }
+
+    @Override
+    protected ThreadFactory getThreadFactory() {
+        return Executors.defaultThreadFactory();
+    }
+}
