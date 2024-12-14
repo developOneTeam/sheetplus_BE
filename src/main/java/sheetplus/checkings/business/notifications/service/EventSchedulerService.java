714eae8eebaf74bb7e60b65624cf51c6e8790325
hwangjeyeon <kingpele9018@gmail.com>
2024-12-15 02:06:56 +0900

    Feat: WAS 실행 시, 스케줄러 초기화 및 업데이트 기능 개발
    - Refactor: 중복 EVENT 스케줄링을 막기 위해 List타입 -> Set타입으로 변경
    - Refactor: SendingStatus 로직 배치 및 @Transactional 적용
    - Feat: 등록시간보다 현재시간이 이전일 경우만 스케줄링하도록 개발
    - Delete: Jobkey, TriggerKey의 Group명칭 삭제
    - Fix: 저장타입 오류 수정
    - Refactor: DB 접근 로직 포함하는 메소드에 @Transactional 추가
    - Refactor: @Transactional 추가에 따른 saveAndflush -> save로 변경
    - Feat: 업데이트 요청의 스케줄링 작업 기능 추가 개발

--- src/main/java/sheetplus/checkings/business/notifications/service/EventSchedulerService.java
+++ src/main/java/sheetplus/checkings/business/notifications/service/EventSchedulerService.java
@@ -21,13 +21,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static sheetplus.checkings.exception.error.ApiError.EVENT_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class EventSchedulerService implements Job {
+public class EventSchedulerService {
 
 
     private final EventRepository eventRepository;
@@ -37,52 +38,19 @@ public class EventSchedulerService implements Job {
     private final Scheduler scheduler;
 
     /**
-     * 매일 12시 30분에 실행되는 스케줄러
-     * 1. 이전날 이벤트 모두 불러와서 태스크 삭제
-     * 2. 당일 이벤트 모두 불러와서 태스크 등록
-     */
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) {
-        // 1. 이전날 이벤트 모두 불러와서 태스크 삭제
-        try {
-            scheduler.clear();
-            log.info("스케줄러에 등록된 모든 이벤트 삭제");
-        } catch (SchedulerException e) {
-            log.info("스케줄러에 등록된 모든 이벤트 삭제도중 예외 발생: {}", e.getMessage());
-            throw new RuntimeException(e);
-        }
-
-        // 2. 당일 이벤트 모두 불러와서 태스크 등록
-        List<Long> contestIds = getAllContestIds();
-        log.info("스케줄러 실행, 스케줄링 대회 수: {} ", contestIds.size());
-        for (Long contestId : contestIds) {
-            updateContestEvents(contestId);
-        }
-    }
-
-    /**
-     * 모든 Contest ID를 가져오는 메서드
-     * 실제 구현에서는 ContestRepository를 통해 가져옵니다.
-     */
-    private List<Long> getAllContestIds() {
-        return contestRepository.findAll().stream()
-                .map(Contest::getId)
-                .toList();
-    }
-
-    /**
      * 특정 Contest의 이벤트를 업데이트
      * 1. 오늘 시작하는 이벤트목록 모두 탐색
      * 2. 이벤트 발송 DB 적재
      * 3. 예약 스케줄 작업 진행
      */
+    @Transactional
     public void updateContestEvents(Long contestId) {
 
         // 1. 오늘 이벤트 조회
         LocalDate today = LocalDate.now();
         LocalDateTime startOfDay = today.atStartOfDay();
         LocalDateTime endOfDay = today.plusDays(1).atStartOfDay().minusSeconds(1);
-        List<Event> todayEvents = eventRepository
+        Set<Event> todayEvents = eventRepository
                 .findByEventContestIdAndStartTimeBetween(contestId, startOfDay, endOfDay);
         log.info("오늘 이벤트 조회, 총 이벤트 수: {}", todayEvents.size());
 
@@ -95,93 +63,6 @@ public class EventSchedulerService implements Job {
         // 3. 알림 스케줄링
         scheduleNotifications(todayEvents);
     }
-
-    public void scheduleEvent(Event event) {
-        EventSending eventBefore = EventSending.builder()
-                .sendingStatus(SendingStatus.PENDING)
-                .eventSendingEvent(event)
-                .sendingType(SendingType.EVENT_BEFORE_10MINUTES)
-                .build();
-        eventSendingRepository.save(eventBefore);
-        EventSending eventNow = EventSending.builder()
-                .sendingStatus(SendingStatus.PENDING)
-                .eventSendingEvent(event)
-                .sendingType(SendingType.EVENT_BEFORE_10MINUTES)
-                .build();
-        eventSendingRepository.saveAndFlush(eventNow);
-    }
-
-    /**
-     * 1. 전달받은 이벤트들을 불러와서 변수로 활용
-     * 2. 이벤트 시작 10분전 알림 작업 + 트리거 설정
-     * 3. 이벤트 시작 알림 작업 + 트리거 설정
-     * 4. 예약 스케줄링 등록
-     */
-    private void scheduleNotifications(List<Event> TodayEvents) {
-        for (Event event : TodayEvents) {
-
-            // 1번 작업
-            LocalDateTime startTime = event.getStartTime();
-            LocalDateTime notificationTimeBefore = startTime.minusMinutes(10);
-            StringBuilder NotificationSchedule = new StringBuilder()
-                    .append(event.getName())
-                    .append("-")
-                    .append(event.getId())
-                    .append("_")
-                    .append(event.getStartTime());
-
-            // 2번 작업 시작 10분 전에 알림
-            JobDetail jobDetailBeforeEvent = JobBuilder.newJob(NotificationService.class)
-                    .withIdentity("EventJob_" + NotificationSchedule + "_BeforeEvent"
-                            , "Notification")
-                    .usingJobData("eventId", event.getId())
-                    .usingJobData("notification Type", "10분 전 알림")
-                    .usingJobData("contestId", event.getEventContest().getId())
-                    .usingJobData("eventName", event.getName())
-                    .build();
-
-            Trigger triggerBeforeEvent = TriggerBuilder.newTrigger()
-                    .forJob(jobDetailBeforeEvent)
-                    .withIdentity("Trigger_" + NotificationSchedule + "_BeforeEvent"
-                            , "Notification")
-                    .startAt(Date.from(notificationTimeBefore
-                            .atZone(ZoneId.systemDefault())
-                            .toInstant()))
-                    .build();
-
-            // 3번 작업 시작 시점 알림
-            JobDetail jobDetailStartEvent = JobBuilder.newJob(NotificationService.class)
-                    .withIdentity("EventJob_" + NotificationSchedule + "_StartEvent"
-                            , "Notification")
-                    .usingJobData("eventId", event.getId())
-                    .usingJobData("notification Type", "이벤트 시작 알림")
-                    .usingJobData("contestId", event.getEventContest().getId())
-                    .usingJobData("eventName", event.getName())
-                    .build();
-
-            Trigger triggerStartEvent = TriggerBuilder.newTrigger()
-                    .forJob(jobDetailStartEvent)
-                    .withIdentity("Trigger_" + NotificationSchedule + "_StartEvent"
-                            , "Notification")
-                    .startAt(Date.from(startTime
-                            .atZone(ZoneId.systemDefault())
-                            .toInstant()))
-                    .build();
-
-            // 4번 작업
-            try {
-                scheduler.scheduleJob(jobDetailBeforeEvent, triggerBeforeEvent);
-                scheduler.scheduleJob(jobDetailStartEvent, triggerStartEvent);
-                log.info("예약 등록 완료");
-            } catch (SchedulerException e) {
-                log.error("스케줄링 예외 발생: {}", e.getMessage());
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-
-
 
     /**
      * 스케줄링 끝난 상태에서 신규 이벤트 생성할 경우
@@ -204,9 +85,7 @@ public class EventSchedulerService implements Job {
         List<EventSending> eventSends = eventSendingRepository
                 .findByEventSendingEvent_Id(eventId);
 
-        if(!eventSends.isEmpty()
-                || eventSends.getFirst().getSendingStatus()
-                .equals(SendingStatus.SENDING)){
+        if(!eventSends.isEmpty()){
             return;
         }
 
@@ -214,56 +93,74 @@ public class EventSchedulerService implements Job {
                 (() -> new ApiException(EVENT_NOT_FOUND));
 
         // 3번 로직
-        scheduleNotifications(Collections.singletonList(event));
-
+        scheduleNotifications(Collections.singleton(event));
 
         // 4번 로직
-        EventSending eventSending = EventSending.builder()
-                .sendingStatus(SendingStatus.PENDING)
-                .eventSendingEvent(event)
-                .build();
-
-        eventSendingRepository.save(eventSending);
+        scheduleEvent(event);
     }
 
 
     /**
      * 스케줄링에 등록한 이벤트를 수정한 경우
      * 1. 스케줄러에 등록한 이벤트 job 삭제
-     * 2. 업데이트된 시간이 오늘 시작하는 이벤트인지 확인
-     * 3. 참이라면 스케줄 예약
-     * 4. 이벤트 발송 DB에 INSERT
-     *
+     * 2. 오늘 시작하는 이벤트인지 확인
+     * 3. 업데이트한 이벤트 스케줄러에 등록
+     * 4. 연결된 이벤트 발송목록 불러와서, SENDING이면 PENDING으로 변경
      */
     @Transactional
     public void scheduleUpdateEvent(EventResponseDto eventResponseDto){
         Long eventId = cryptoUtil.decrypt(eventResponseDto.getSecureId());
+        Event event = eventRepository.findById(eventId).orElseThrow
+                (() -> new ApiException(EVENT_NOT_FOUND));
         // 1번 로직
-        try {
-            scheduler.deleteJob(JobKey.jobKey(eventId.toString()));
-        } catch (SchedulerException e) {
-            log.error("수정요청 작업 삭제중 예외 발생: {}",e.getMessage());
-            throw new RuntimeException(e);
+        deleteJob(event);
+
+        // 2번 로직
+        if(!eventResponseDto.getStartTime().toLocalDate()
+                .isEqual(LocalDate.now())){
+            return;
         }
 
-        scheduleNewEvent(eventResponseDto);
+        // 3번 로직
+        scheduleNotifications(Collections.singleton(event));
 
+        // 4번 로직
+        List<EventSending> eventSends = eventSendingRepository
+                .findByEventSendingEvent_Id(eventId);
+
+        for (EventSending eventSend : eventSends) {
+            // 발송 상태가 SENDING인 경우 PENDING 변경 필요
+            if(eventSend.getSendingStatus().equals(SendingStatus.SENDING)){
+                // 현재시간이 등록시간보다 이전이면 PENDING으로 변경
+
+                // 시작 10분전 알림 설정
+                if(LocalDateTime.now().isBefore(eventResponseDto.getStartTime().minusMinutes(10))
+                        && eventSend.getSendingType().equals(SendingType.EVENT_BEFORE_10MINUTES)){
+                    eventSend.changeStatus(SendingStatus.PENDING);
+                    continue;
+                }
+
+                // 시작 알림 설정
+                if(LocalDateTime.now().isBefore(eventResponseDto.getStartTime())
+                        && eventSend.getSendingType().equals(SendingType.EVENT_START_NOW)){
+                    eventSend.changeStatus(SendingStatus.PENDING);
+                }
+            }
+        }
     }
+
 
     /**
      * 스케줄링에 등록한 이벤트를 삭제한 경우
      * 1. 스케줄러에 등록한 이벤트 작업 삭제
-     *
      */
     @Transactional
     public void scheduleDeleteEvent(Long eventId){
         // 1번 로직
-        try {
-            scheduler.deleteJob(JobKey.jobKey(eventId.toString()));
-        } catch (SchedulerException e) {
-            log.error("삭제요청 작업 삭제중 예외 발생: {}",e.getMessage());
-            throw new RuntimeException(e);
-        }
+        Event event = eventRepository.findById(eventId).orElseThrow
+                (() -> new ApiException(EVENT_NOT_FOUND));
+        deleteJob(event);
+        eventSendingRepository.deleteByEventSendingEvent_Id(eventId);
     }
 
     /**
@@ -271,12 +168,146 @@ public class EventSchedulerService implements Job {
      */
     @Transactional
     public void initializeScheduler(){
-        List<Event> events = eventSendingRepository
+        Set<Event> events = eventSendingRepository
                 .findBySendingStatusNot(SendingStatus.SENDING)
                 .stream()
                 .map(EventSending::getEventSendingEvent)
-                        .toList();
+                .collect(Collectors.toSet());
+
         scheduleNotifications(events);
+        log.info("스케줄러 초기화 작업 완료");
+    }
+
+    @Transactional
+    public void scheduleEvent(Event event) {
+        EventSending eventBefore = EventSending.builder()
+                .sendingStatus(SendingStatus.PENDING)
+                .eventSendingEvent(event)
+                .sendingType(SendingType.EVENT_BEFORE_10MINUTES)
+                .build();
+        eventSendingRepository.save(eventBefore);
+        EventSending eventNow = EventSending.builder()
+                .sendingStatus(SendingStatus.PENDING)
+                .eventSendingEvent(event)
+                .sendingType(SendingType.EVENT_START_NOW)
+                .build();
+        eventSendingRepository.save(eventNow);
+    }
+
+    /**
+     * 모든 Contest ID를 가져오는 메서드
+     */
+    @Transactional
+    public List<Long> getAllContestIds() {
+        return contestRepository.findAll()
+                .stream()
+                .map(Contest::getId)
+                .toList();
+    }
+
+
+    /**
+     * 1. 전달받은 이벤트들을 불러와서 변수로 활용
+     * 2. 이벤트 시작 10분전 알림 작업 + 트리거 설정
+     * 3. 이벤트 시작 알림 작업 + 트리거 설정
+     * 4. 예약 스케줄링 등록
+     */
+    private void scheduleNotifications(Set<Event> TodayEvents) {
+        for (Event event : TodayEvents) {
+
+            // 1번 작업
+            LocalDateTime startTime = event.getStartTime();
+            LocalDateTime notificationTimeBefore = startTime.minusMinutes(10);
+            StringBuilder NotificationSchedule = new StringBuilder()
+                    .append(event.getName())
+                    .append("-")
+                    .append(event.getId())
+                    .append("_")
+                    .append(event.getStartTime());
+
+            // 2번 작업 시작 10분 전에 알림
+            JobDetail jobDetailBeforeEvent = JobBuilder.newJob(NotificationService.class)
+                    .withIdentity("EventJob_" + NotificationSchedule + "_BeforeEvent")
+                    .usingJobData("eventId", event.getId())
+                    .usingJobData("notification Type", "10분 전 알림")
+                    .usingJobData("contestId", event.getEventContest().getId())
+                    .usingJobData("eventName", event.getName())
+                    .build();
+
+            Trigger triggerBeforeEvent = TriggerBuilder.newTrigger()
+                    .forJob(jobDetailBeforeEvent)
+                    .withIdentity("Trigger_" + NotificationSchedule + "_BeforeEvent")
+                    .startAt(Date.from(notificationTimeBefore
+                            .atZone(ZoneId.systemDefault())
+                            .toInstant()))
+                    .build();
+
+            // 3번 작업 시작 시점 알림
+            JobDetail jobDetailStartEvent = JobBuilder.newJob(NotificationService.class)
+                    .withIdentity("EventJob_" + NotificationSchedule + "_StartEvent")
+                    .usingJobData("eventId", event.getId())
+                    .usingJobData("notification Type", "이벤트 시작 알림")
+                    .usingJobData("contestId", event.getEventContest().getId())
+                    .usingJobData("eventName", event.getName())
+                    .build();
+
+            Trigger triggerStartEvent = TriggerBuilder.newTrigger()
+                    .forJob(jobDetailStartEvent)
+                    .withIdentity("Trigger_" + NotificationSchedule + "_StartEvent")
+                    .startAt(Date.from(startTime
+                            .atZone(ZoneId.systemDefault())
+                            .toInstant()))
+                    .build();
+
+            // 4번 작업
+            try {
+                if(LocalDateTime.now().isBefore(notificationTimeBefore)){
+                    scheduler.scheduleJob(jobDetailBeforeEvent, triggerBeforeEvent);
+                    log.info("10분전 알림 등록: {}", notificationTimeBefore);
+                }
+
+                if(LocalDateTime.now().isBefore(startTime)){
+                    scheduler.scheduleJob(jobDetailStartEvent, triggerStartEvent);
+                    log.info("시작 알림 등록: {}", startTime);
+                }
+            } catch (SchedulerException e) {
+                log.error("스케줄링 예외 발생: {}", e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void deleteJob(Event event){
+        try {
+            StringBuilder deleteScheduleJob = new StringBuilder()
+                    .append(event.getName())
+                    .append("-")
+                    .append(event.getId())
+                    .append("_")
+                    .append(event.getStartTime());
+
+            scheduler.deleteJob(JobKey
+                    .jobKey("EventJob_" + deleteScheduleJob + "_BeforeEvent"));
+            scheduler.pauseTrigger(TriggerKey
+                    .triggerKey("Trigger_" + deleteScheduleJob + "_BeforeEvent"));
+            scheduler.deleteJob(JobKey
+                    .jobKey("EventJob_" + deleteScheduleJob + "_StartEvent"));
+            scheduler.pauseTrigger(TriggerKey
+                    .triggerKey("Trigger_" + deleteScheduleJob + "_StartEvent"));
+
+            log.info("이벤트 삭제 완료");
+        } catch (SchedulerException e) {
+            log.error("삭제요청 작업 삭제중 예외 발생: {}",e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional
+    public void sendStatusChange(Long eventId, SendingType type) {
+        EventSending eventSending = eventSendingRepository
+                .findByEventSendingEvent_IdAndSendingType(eventId, type);
+        eventSending.changeStatus(SendingStatus.SENDING);
+        eventSendingRepository.saveAndFlush(eventSending);
     }
 
 }
