ad8ea3a3a5545c436311c57f7e5f88885de398cf
hwangjeyeon <kingpele9018@gmail.com>
2024-12-14 13:46:38 +0900

    Feat: EventSending Repository 개발

---
+++ src/main/java/sheetplus/checkings/domain/eventSending/repository/EventSendingRepository.java
@@ -0,0 +1,15 @@
+package sheetplus.checkings.domain.eventSending.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sheetplus.checkings.domain.enums.SendingStatus;
+import sheetplus.checkings.domain.enums.SendingType;
+import sheetplus.checkings.domain.eventSending.entity.EventSending;
+
+import java.util.List;
+
+public interface EventSendingRepository extends JpaRepository<EventSending, Long> {
+
+    List<EventSending> findByEventSendingEvent_Id(Long eventId);
+    EventSending findByEventSendingEvent_IdAndSendingType(Long eventId, SendingType sendingType);
+    List<EventSending> findBySendingStatusNot(SendingStatus sendingStatus);
+}
