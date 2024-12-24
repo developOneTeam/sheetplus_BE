5a81dcf50265a65c4f3e6fa70460c5b73ac999b1
hwangjeyeon <kingpele9018@gmail.com>
2024-12-24 16:59:45 +0900

    Feat: QrCode 생성데이터 조회 컨트롤러 앤드포인트 개발

--- src/main/java/sheetplus/checkings/business/qrcode/controller/QrController.java
+++ src/main/java/sheetplus/checkings/business/qrcode/controller/QrController.java
@@ -3,6 +3,7 @@ package sheetplus.checkings.business.qrcode.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import sheetplus.checkings.business.qrcode.dto.QrcodeCreateResponseDto;
 import sheetplus.checkings.business.qrcode.dto.QrcodeRequestDto;
 import sheetplus.checkings.business.qrcode.dto.QrcodeResponseDto;
 import sheetplus.checkings.business.qrcode.service.QrcodeService;
@@ -11,12 +12,12 @@ import sheetplus.checkings.util.response.Api;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("private/student")
+@RequestMapping("private")
 public class QrController {
 
     private final QrcodeService qrcodeService;
 
-    @PostMapping("/qrcode/check")
+    @PostMapping("/student/qrcode/check")
     public Api<QrcodeResponseDto> qrcodeCheck(
             @RequestHeader(value = "Authorization", required = false) String token,
             @RequestBody QrcodeRequestDto qrcodeRequestDto){
@@ -28,5 +29,16 @@ public class QrController {
         return Api.OK(qrcodeResponseDto);
     }
 
+    @GetMapping("/admin/qrcode/create/{secureId}")
+    public Api<QrcodeCreateResponseDto> createQrcode(
+            @RequestHeader(value = "Authorization", required = false) String token,
+            @PathVariable(name = "secureId") String secureId){
+
+        QrcodeCreateResponseDto qrcodeCreateResponseDto
+                = qrcodeService.createQrcode(
+                        token.replace("Bearer","").trim(), secureId);
+
+        return Api.OK(qrcodeCreateResponseDto);
+    }
 
 }
