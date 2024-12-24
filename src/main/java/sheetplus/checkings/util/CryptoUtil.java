3da31dd60c34a12b58c6762c2d8799c22d664282
hwangjeyeon <kingpele9018@gmail.com>
2024-12-24 16:58:14 +0900

    Feat: SecretKey Getter추가

--- src/main/java/sheetplus/checkings/util/CryptoUtil.java
+++ src/main/java/sheetplus/checkings/util/CryptoUtil.java
@@ -1,6 +1,7 @@
 package sheetplus.checkings.util;
 
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -25,6 +26,7 @@ public class CryptoUtil {
     @Value("${crypto.algorithm}")
     private String ALGORITHM;
 
+    @Getter
     @Value("${crypto.secret-key}")
     private String SECRET_KEY;
 
