fb6d60de9bcb3f5bf9d8e147328d55d0adb7075d
hwangjeyeon <kingpele9018@gmail.com>
2024-12-24 16:33:52 +0900

    Feat: 만료시간 검증 로직 개발
    - 15초 + 추가 고려시간(2초 / 네트워크 지연 등)을 현재시간과 비교

--- src/main/java/sheetplus/checkings/business/qrcode/service/QrcodeService.java
+++ src/main/java/sheetplus/checkings/business/qrcode/service/QrcodeService.java
@@ -22,6 +22,8 @@ import sheetplus.checkings.exception.exceptionMethod.ApiException;
 import sheetplus.checkings.util.CryptoUtil;
 import sheetplus.checkings.util.JwtUtil;
 
+import java.time.LocalDateTime;
+
 import static sheetplus.checkings.exception.error.ApiError.*;
 
 @RequiredArgsConstructor
@@ -39,14 +41,15 @@ public class QrcodeService {
      *
      * QR코드 인증 로직 Flow
      * 1. Event PK 복호화
-     * 2. Member, Event 조회
-     * 3. EventType 체크 - CHECKING 타입인지 확인 / 아닐 경우 QR_NOT_VALID 예외 발생
-     * 4. Event와 연관관계 맺은 Contest 정보 조회
-     * 5. Event와 Contest EVENT_PROGRESS 상태인지 확인 / 아닐 경우 EVENT_NOT_PROGRESS 예외 발생
-     * 6. Member, Contest 정보로 participateContest 조회 - 없을 경우 null Return
-     * 7. participateContest이 null인 경우, participateContest객체 생성 및 연관관계 설정 후 SAVE
-     * 8. participateContest이 null이 아닌 경우, Event의 타입 포함여부 확인 - 포함된 경우 EVENT_ALREADY_PARTICIPATE 예외 발생
-     * 9. 8번 이후, 참여대회 수, 참여 대회 타입 저장 - 변경감지로 DB UPDATE 반영
+     * 2. 만료시간 복호화 및 검증 - 지정 시간을 벗어나거나 복호화 실패할 경우 예외 발생
+     * 3. Member, Event 조회
+     * 4. EventType 체크 - CHECKING 타입인지 확인 / 아닐 경우 QR_NOT_VALID 예외 발생
+     * 5. Event와 연관관계 맺은 Contest 정보 조회
+     * 6 Event와 Contest EVENT_PROGRESS 상태인지 확인 / 아닐 경우 EVENT_NOT_PROGRESS 예외 발생
+     * 7. Member, Contest 정보로 participateContest 조회 - 없을 경우 null Return
+     * 8. participateContest이 null인 경우, participateContest객체 생성 및 연관관계 설정 후 SAVE
+     * 9. participateContest이 null이 아닌 경우, Event의 타입 포함여부 확인 - 포함된 경우 EVENT_ALREADY_PARTICIPATE 예외 발생
+     * 10. 8번 이후, 참여대회 수, 참여 대회 타입 저장 - 변경감지로 DB UPDATE 반영
      *
      * @param token - 사용자 정보
      * @param qrcodeRequestDto - 암호화된 Event 엔티티 PK
@@ -58,38 +61,45 @@ public class QrcodeService {
         Long id = cryptoUtil.decrypt(qrcodeRequestDto.getSecureCode());
 
         // 2번 로직
+        LocalDateTime expireTime = cryptoUtil
+                .decryptExpireTime(qrcodeRequestDto.getSecureExpireTime());
+        if(LocalDateTime.now().plusSeconds(17).isAfter(expireTime)){
+            throw new ApiException(EXPIRED_QR_CODES);
+        }
+
+        // 3번 로직
         Member member = memberRepository.findById(jwtUtil.getMemberId(token))
                 .orElseThrow(() -> new ApiException(MEMBER_NOT_FOUND));
         Event event = eventRepository.findById(id)
                 .orElseThrow(() -> new ApiException(EVENT_NOT_FOUND));
 
-        // 3번 로직
+        // 4번 로직
         if(event.getEventType().equals(EventType.NO_CHECKING)){
             throw new ApiException(QR_NOT_VALID);
         }
 
-        // 4번 로직
+        // 5번 로직
         Contest contest = event.getEventContest();
 
-        // 5번 로직
+        // 6번 로직
         if(!contest.getCons().equals(ContestCons.EVENT_PROGRESS)
             || !event.getEventCondition().equals(ContestCons.EVENT_PROGRESS)){
             throw new ApiException(EVENT_NOT_PROGRESS);
         }
 
-        // 6번 로직
+        // 7번 로직
         ParticipateContest participateContest = participateContestStateRepository
                 .findByMemberParticipateContestState_IdAndContestParticipateContestState_Id(
                         member.getId(), contest.getId())
-                .orElseGet(() -> createNewParticipateContest(contest, member, event)); // 7번 로직
+                .orElseGet(() -> createNewParticipateContest(contest, member, event)); // 8번 로직
 
         if(participateContest != null){
-            // 8번 로직
+            // 9번 로직
             if(participateContest.getEventTypeSet().contains(event.getEventCategory())){
                 throw new ApiException(EVENT_ALREADY_PARTICIPATE);
             }
 
-            // 9번 로직
+            // 10번 로직
             updateParticipateContest(participateContest, event);
         }
 
@@ -100,7 +110,7 @@ public class QrcodeService {
                 .build();
     }
 
-    // 9번 로직
+    // 10번 로직
     private static void updateParticipateContest(ParticipateContest participateContest, Event event) {
         participateContest.addCounts();
         participateContest.getEventTypeSet().add(event.getEventCategory());
@@ -110,7 +120,7 @@ public class QrcodeService {
                 participateContest.getEventTypeSet());
     }
 
-    // 7번 로직
+    // 8번 로직
     private ParticipateContest createNewParticipateContest(Contest contest, Member member, Event event) {
         ParticipateContest participateContest = ParticipateContest.builder()
                 .eventsCount(1)
@@ -126,6 +136,5 @@ public class QrcodeService {
                 event.getName(), event.getEventType());
         return null;
     }
-
 
 }
