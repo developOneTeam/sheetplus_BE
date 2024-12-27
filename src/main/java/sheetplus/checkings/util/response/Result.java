0aa802979e14e3b403ac95033e5e83b865fec839
hwangjeyeon <kingpele9018@gmail.com>
2024-12-28 00:46:48 +0900

    Refactor: 정상응답반환 메소드 삭제, 필요한 ERROR 타입만 추가

--- src/main/java/sheetplus/checkings/util/response/Result.java
+++ src/main/java/sheetplus/checkings/util/response/Result.java
@@ -3,71 +3,21 @@ package sheetplus.checkings.util.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import sheetplus.checkings.exception.error.ErrorCode;
-import sheetplus.checkings.exception.error.ErrorCodeIfs;
+import org.springframework.http.HttpStatus;
+
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class Result {
 
-    private Integer resultCode;
+    private HttpStatus resultStatus;
     private String resultMessage;
 
-    public static Result OK() {
+    public static Result ERROR(HttpStatus httpStatus, String message){
         return Result.builder()
-                .resultCode(ErrorCode.OK.getHttpStatusCode())
-                .resultMessage(ErrorCode.OK.getErrorDescription())
-                .build();
-    }
-
-    public static Result CREATED() {
-        return Result.builder()
-                .resultCode(ErrorCode.CREATED.getHttpStatusCode())
-                .resultMessage(ErrorCode.CREATED.getErrorDescription())
-                .build();
-    }
-
-    public static Result READ() {
-        return Result.builder()
-                .resultCode(ErrorCode.READ.getHttpStatusCode())
-                .resultMessage(ErrorCode.READ.getErrorDescription())
-                .build();
-    }
-
-    public static Result UPDATED() {
-        return Result.builder()
-                .resultCode(ErrorCode.UPDATED.getHttpStatusCode())
-                .resultMessage(ErrorCode.UPDATED.getErrorDescription())
-                .build();
-    }
-
-    public static Result DELETED() {
-        return Result.builder()
-                .resultCode(ErrorCode.DELETED.getHttpStatusCode())
-                .resultMessage(ErrorCode.DELETED.getErrorDescription())
-                .build();
-    }
-
-    public static Result ERROR(ErrorCodeIfs errorCodeIfs){
-        return Result.builder()
-                .resultCode(errorCodeIfs.getHttpStatusCode())
-                .resultMessage(errorCodeIfs.getErrorDescription())
-                .build()
-                ;
-    }
-
-
-    // validation용 ERROR
-    public static Result ERROR(
-            Integer errorCode,
-            String errorDescription
-    ){
-        return Result.builder()
-                .resultCode(errorCode)
-                .resultMessage(errorDescription)
+                .resultStatus(httpStatus)
+                .resultMessage(message)
                 .build();
     }
 
