bb6b86afefd4aae489cead5d016dfcbe729b1126
hwangjeyeon <kingpele9018@gmail.com>
2025-01-01 21:49:01 +0900

    Feat: Request Validation 예외처리 기능 개발

--- src/main/java/sheetplus/checkings/exception/handler/ApiExceptionHandler.java
+++ src/main/java/sheetplus/checkings/exception/handler/ApiExceptionHandler.java
@@ -1,9 +1,12 @@
 package sheetplus.checkings.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sheetplus.checkings.exception.error.ErrorResponse;
@@ -46,12 +49,25 @@ public class ApiExceptionHandler {
     public ResponseEntity<ErrorResponse> illegalArgumentExceptionHandler(
             IllegalArgumentException e
     ){
-        log.info("{}", e.getMessage());
+        log.error("{}", e.getMessage());
 
         return ResponseEntity
                 .status(HTTP_INPUT_NOT_READABLE.getHttpStatusCode())
                 .body(ErrorResponse.ERROR(HTTP_INPUT_NOT_READABLE.getHttpStatusCode(),
                         HTTP_INPUT_NOT_READABLE.getErrorDescription()));
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> constraintDeclarationExceptionHandler(
+            MethodArgumentNotValidException e
+    ){
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.ERROR(HttpStatus.BAD_REQUEST,
+                        e.getBindingResult().getFieldErrors().stream()
+                                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                .toList().toString()));
     }
 
 }
