afbdcb2d88d7189b2cb77a88e1ba8486da914514
hwangjeyeon <kingpele9018@gmail.com>
2025-01-04 01:06:52 +0900

    Docs: Favorite API 문서화 작업
    - URI 설명 구체화
    - DTO 설명 구체화

--- src/main/java/sheetplus/checkings/domain/favorite/controller/FavoriteController.java
+++ src/main/java/sheetplus/checkings/domain/favorite/controller/FavoriteController.java
@@ -1,5 +1,13 @@
 package sheetplus.checkings.domain.favorite.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -11,6 +19,7 @@ import sheetplus.checkings.domain.favorite.dto.FavoriteDto.FavoriteCreateRespons
 import sheetplus.checkings.domain.favorite.dto.FavoriteDto.FavoriteResponseDto;
 import sheetplus.checkings.domain.favorite.dto.FavoriteDto.SubScribeResponseDTO;
 import sheetplus.checkings.domain.favorite.service.FavoriteCRUDService;
+import sheetplus.checkings.exception.error.ErrorResponse;
 
 import java.util.List;
 
@@ -18,12 +27,32 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("private/student")
+@Tag(name = "Favorite", description = "Favorite Service API")
 public class FavoriteController {
 
     private final FavoriteCRUDService favoriteCRUDService;
 
     @PostMapping("/favorite/v1")
+    @Operation(summary = "Favorite CREATE", description = "즐겨찾기 이벤트를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "이벤트 즐겨찾기 생성 및 FCM 구독등록 진행",
+                    content = @Content(schema = @Schema(implementation = FavoriteCreateResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "요청한 입력값이 지정된 검증을 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Member or Contest or Event] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "409", description = "이미 등록된 즐겨찾기 이벤트입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
     public ResponseEntity<FavoriteCreateResponseDto> createFavorite(
+            @Parameter(description = "액세스 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "Authorization", required = false) String token,
             @RequestBody @Validated FavoriteRequestDto favoriteRequestDto) {
 
@@ -37,11 +66,34 @@ public class FavoriteController {
 
 
     @GetMapping("/contests/{contest}/favorites/v1")
+    @Operation(summary = "Favorite GET", description = "즐겨찾기 이벤트를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "이벤트 즐겨찾기 조회 성공",
+                    content = @Content(array = @ArraySchema(schema =
+                    @Schema(implementation = FavoriteResponseDto.class)),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "잘못된 HTTP 입력 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "구독 상태정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+    })
     public ResponseEntity<List<FavoriteResponseDto>> getFavorites(
+            @Parameter(description = "액세스 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "Authorization", required = false) String token,
+
+            @Parameter(description = "Contest PK", example = "1")
             @PathVariable(name = "contest") Long contestId,
+
+            @Parameter(description = "조회할 페이지 번호", example = "1")
             @RequestParam(value = "offset", required = false)
             Integer offset,
+
+            @Parameter(description = "페이지당 조회할 데이터 개수", example = "1")
             @RequestParam(value = "limit", required = false)
             Integer limit
             ){
@@ -54,18 +106,59 @@ public class FavoriteController {
 
 
     @DeleteMapping("/favorites/{favorite}/devices/{deviceToken}/v1")
+    @Operation(summary = "Favorite DELETE", description = "FCM 구독삭제 진행, 삭제 성공하면 이벤트 즐겨찾기 삭제")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "FCM 구독삭제 진행",
+                    content = @Content(schema = @Schema(implementation = FavoriteResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "잘못된 HTTP 입력 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Member or Event or Favorite or 구독 상태] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+    })
     public ResponseEntity<Void> deleteFavorite(
+            @Parameter(description = "액세스 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "Authorization", required = false) String token,
+
+            @Parameter(description = "Favorite PK", example = "1")
             @PathVariable(name = "favorite") Long id,
+
+            @Parameter(description = "DeviceToken - FCM에서 발급한 유저 디바이스 토큰입니다.",
+                    example = "test123device4567token")
             @PathVariable(name = "deviceToken") String deviceToken
     ){
         favoriteCRUDService.deleteFavorites(id, token.replace("Bearer ", ""), deviceToken);
         return ResponseEntity.accepted().build();
     }
 
+
     @GetMapping("/favorites/{favorite}/status/{statusType}/v1")
+    @Operation(summary = "Favorite STATUS", description = "FCM 구독작업의 상태를 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "FCM 구독작업 진행상태 조회 성공",
+                    content = @Content(array = @ArraySchema(schema =
+                    @Schema(implementation = FavoriteResponseDto.class)),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "잘못된 HTTP 입력 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[구독 상태] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+    })
     public ResponseEntity<SubScribeResponseDTO> favoriteStatusCheck(
+            @Parameter(description = "Favorite PK")
             @PathVariable(name = "favorite") Long id,
+
+            @Parameter(description = "구독작업 유형 [subscribe or unsubscribe]")
             @PathVariable(name = "statusType") String type
     ){
         SubScribeResponseDTO subScribeResponseDTO = favoriteCRUDService.getFavoriteStatus(id, type);
