b0885dcff35c71180bd6ec46978b6f33fcb15ab7
hwangjeyeon <kingpele9018@gmail.com>
2025-01-04 01:06:41 +0900

    Docs: Authentication API 문서화 작업
    - URI 설명 구체화
    - DTO 설명 구체화

--- src/main/java/sheetplus/checkings/business/auth/controller/AuthController.java
+++ src/main/java/sheetplus/checkings/business/auth/controller/AuthController.java
@@ -1,5 +1,13 @@
 package sheetplus.checkings.business.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,26 +18,65 @@ import sheetplus.checkings.domain.member.dto.MemberDto.MemberLoginRequestDto;
 import sheetplus.checkings.domain.token.dto.TokenDto;
 import sheetplus.checkings.business.auth.service.AuthService;
 import sheetplus.checkings.business.email.service.EmailService;
+import sheetplus.checkings.exception.error.ErrorResponse;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("public/auth")
 @Slf4j
+@Tag(name = "Authentication", description = "Authentication Service API")
 public class AuthController {
     public final AuthService authService;
     private final EmailService emailService;
 
     @PostMapping("/token/refresh/v1")
+    @Operation(summary = "Access-Token refresh with Refresh-Token",
+            description = "Access-Token과 Refresh-Token을 재발급합니다.",
+            security = {@SecurityRequirement(name = "accessToken"),
+                    @SecurityRequirement(name = "refreshToken")
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "토큰 생성"
+                    , content = {@Content(schema = @Schema(implementation = TokenDto.class),
+                    mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 토큰이거나 찾을 수 없습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
     public ResponseEntity<TokenDto> refreshToken(
+            @Parameter(description = "리프래쉬 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "refreshToken"
                     , required = false) String refreshToken){
         log.info("{}", refreshToken);
 
         TokenDto tokenDto = authService.refreshTokens(refreshToken);
-        return ResponseEntity.ok(tokenDto);
+        return ResponseEntity.created(URI.create(""))
+                .body(tokenDto);
     }
 
     @PostMapping("/login/v1")
+    @Operation(summary = "Member login", description = "학교이메일로 Member 로그인을 진행합니다.",
+                security = @SecurityRequirement(name = "accessToken"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공",
+                    content = {@Content(schema = @Schema(implementation = TokenDto.class),
+                    mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "요청한 입력값이 지정된 검증을 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "인증된 인증코드가 아닙니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "임시멤버정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "409", description = "요청한 멤버 타입과 회원가입한 멤버의 타입이 일치하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
     public ResponseEntity<TokenDto> loginMember(
             @RequestBody @Validated MemberLoginRequestDto memberLoginRequestDto){
 
--- src/main/java/sheetplus/checkings/domain/token/dto/TokenDto.java
+++ src/main/java/sheetplus/checkings/domain/token/dto/TokenDto.java
@@ -1,15 +1,20 @@
 package sheetplus.checkings.domain.token.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import sheetplus.checkings.domain.member.dto.MemberDto.MemberInfoResponseDto;
 
 @Getter @Setter
 @Builder @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Token Dto", contentMediaType = "application/json")
 public class TokenDto {
 
+    @Schema(description = "액세스 토큰; 인증 API 요청에 필수적입니다.")
     private String accessToken;
+    @Schema(description = "리프래쉬 토큰; 액세스 토큰이 만료되면, 리프래쉬 토큰으로 재발급받습니다.")
     private String refreshToken;
+    @Schema(implementation = MemberInfoResponseDto.class)
     private MemberInfoResponseDto memberInfo;
 
 }
