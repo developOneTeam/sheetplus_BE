6bbfd7dc3e83dab9da52dd65583e9a2edac52144
hwangjeyeon <kingpele9018@gmail.com>
2025-01-04 16:44:53 +0900

    Docs: Draw API 문서화 작업
    - URI 설명 구체화
    - DTO 설명 구체화

--- src/main/java/sheetplus/checkings/domain/draw/controller/DrawController.java
+++ src/main/java/sheetplus/checkings/domain/draw/controller/DrawController.java
@@ -1,5 +1,12 @@
 package sheetplus.checkings.domain.draw.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +18,7 @@ import sheetplus.checkings.domain.draw.dto.DrawDto.DrawUpdateRequestDto;
 import sheetplus.checkings.domain.draw.dto.DrawDto.DrawUpdateResponseDto;
 import sheetplus.checkings.domain.draw.service.DrawEventService;
 import sheetplus.checkings.deprecated.prize.PrizeConditionRequestDto;
+import sheetplus.checkings.exception.error.ErrorResponse;
 
 import java.net.URI;
 
@@ -18,6 +26,7 @@ import java.net.URI;
 @RequiredArgsConstructor
 @RequestMapping("private/admin")
 @Slf4j
+@Tag(name = "Draw", description = "Draw CUD API")
 public class DrawController {
 
     private final DrawEventService drawEventService;
@@ -38,6 +47,24 @@ public class DrawController {
     }
 
     @PostMapping("/draw/v1")
+    @Operation(summary = "Draw CREATE", description = "추첨 이벤트를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "추첨 이벤트를 생성했습니다",
+                    content = @Content(schema = @Schema(implementation = DrawEventResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "요청한 입력값이 지정된 검증을 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "접근 권한이 없는 사용자입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Member or Contest] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
     public ResponseEntity<DrawEventResponseDto> createDraw(
             @RequestBody @Validated DrawEventRequestDto drawEventRequestDto
     ){
@@ -46,6 +73,24 @@ public class DrawController {
     }
 
     @PutMapping("/draw/v1")
+    @Operation(summary = "Draw UPDATE", description = "추첨 Event Member의 수령 여부를 업데이트합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "추첨 Event Member의 수령 여부를 업데이트했습니다.",
+                    content = @Content(schema = @Schema(implementation = DrawUpdateResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "요청한 입력값이 지정된 검증을 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "접근 권한이 없는 사용자입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Draw] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
     public ResponseEntity<DrawUpdateResponseDto> updateDrawEventReceiveCondition(
             @RequestBody @Validated DrawUpdateRequestDto drawUpdateRequestDto
             ){
@@ -54,7 +99,24 @@ public class DrawController {
     }
     
     @DeleteMapping("/draw/{draw}/v1")
-    public ResponseEntity<String> deleteDrawEventReceiveCondition(
+    @Operation(summary = "Draw DELETE", description = "추첨 Event Member의 수령 내역을 삭제합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "추첨 Event Member의 수령 내역을 삭제했습니다"),
+            @ApiResponse(responseCode = "400", description = "잘못된 HTTP 입력 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "접근 권한이 없는 사용자입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Draw] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
+    public ResponseEntity<Void> deleteDrawEventReceiveCondition(
+            @Parameter(description = "Draw PK", example = "1")
             @PathVariable(name = "draw") Long id
     ){
         drawEventService.deleteDraw(id);
