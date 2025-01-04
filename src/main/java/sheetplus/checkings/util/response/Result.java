2369d5681a9daa5a9196e0d7923d4ea37f675863
hwangjeyeon <kingpele9018@gmail.com>
2025-01-04 22:14:53 +0900

    Docs: Error Dto 필드설명 명세

--- src/main/java/sheetplus/checkings/util/response/Result.java
+++ src/main/java/sheetplus/checkings/util/response/Result.java
@@ -1,5 +1,6 @@
 package sheetplus.checkings.util.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,12 @@ import org.springframework.http.HttpStatus;
 @Data
 @AllArgsConstructor
 @Builder
+@Schema(name="Error Result", description = "예외발생 메세지")
 public class Result {
 
+    @Schema(description = "응답 상태코드의 상태를 표시합니다", example = "BAD_REQUEST")
     private HttpStatus resultStatus;
+    @Schema(description = "서버 내부적으로 개발한 예외 메세지를 출력합니다.", example = "잘못된 HTTP 입력 요청")
     private String resultMessage;
 
     public static Result ERROR(HttpStatus httpStatus, String message){
