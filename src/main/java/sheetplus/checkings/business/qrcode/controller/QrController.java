1a7b3ff861268d8b368e718721eb3f3c75b24582
hwangjeyeon <kingpele9018@gmail.com>
2025-01-04 13:13:55 +0900

    Docs: QRcode API 문서화 작업
    - URI 설명 구체화
    - DTO 설명 구체화

--- src/main/java/sheetplus/checkings/business/qrcode/controller/QrController.java
+++ src/main/java/sheetplus/checkings/business/qrcode/controller/QrController.java
@@ -1,5 +1,12 @@
 package sheetplus.checkings.business.qrcode.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +16,40 @@ import sheetplus.checkings.business.qrcode.dto.QrCodeDto.QrcodeCreateResponseDto
 import sheetplus.checkings.business.qrcode.dto.QrCodeDto.QrcodeRequestDto;
 import sheetplus.checkings.business.qrcode.dto.QrCodeDto.QrcodeResponseDto;
 import sheetplus.checkings.business.qrcode.service.QrcodeService;
+import sheetplus.checkings.exception.error.ErrorResponse;
 
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("private")
+@Tag(name = "QRcode", description = "QRcode Service API")
 public class QrController {
 
     private final QrcodeService qrcodeService;
 
     @PostMapping("/student/qrcode/v1")
+    @Operation(summary = "QRcode register", description = "QRcode 인증을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "QRcode 인증을 성공했습니다. 참여자로 등록합니다.",
+                    content = @Content(schema = @Schema(implementation = QrcodeResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "요청한 입력값이 지정된 검증을 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다, 이미 만료된 QR코드 요청입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Member or Event] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "409",
+                    description = "QRcode를 인증 이벤트가 아닙니다, 현재 진행중인 이벤트가 아닙니다, 이미 참여한 행사입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
     public ResponseEntity<QrcodeResponseDto> qrcodeCheck(
+            @Parameter(description = "액세스 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "Authorization", required = false) String token,
             @RequestBody @Validated QrcodeRequestDto qrcodeRequestDto){
 
@@ -32,8 +61,32 @@ public class QrController {
     }
 
     @GetMapping("/admin/events/{eventId}/qrcode/v1")
+    @Operation(summary = "QRcode CREATE", description = "QRcode를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "QRcode를 생성합니다",
+                    content = @Content(schema = @Schema(implementation = QrcodeCreateResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "잘못된 HTTP 입력 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다, 이미 만료된 QR코드 요청입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "접근 권한이 없는 사용자입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Member or Event] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "409", description = "QRcode를 인증 이벤트가 아닙니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
     public ResponseEntity<QrcodeCreateResponseDto> createQrcode(
+            @Parameter(description = "액세스 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "Authorization", required = false) String token,
+
+            @Parameter(description = "Event PK", example = "1")
             @PathVariable(name = "eventId") Long id){
 
         QrcodeCreateResponseDto qrcodeCreateResponseDto
--- src/main/java/sheetplus/checkings/business/qrcode/dto/QrCodeDto.java
+++ src/main/java/sheetplus/checkings/business/qrcode/dto/QrCodeDto.java
@@ -1,5 +1,6 @@
 package sheetplus.checkings.business.qrcode.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -18,7 +19,11 @@ public class QrCodeDto {
     @Builder
     @NoArgsConstructor @AllArgsConstructor
     public static class QrcodeCreateResponseDto{
+        @Schema(description = "암호화된 Event PK",
+                example = "987secure6543Id", type = "String")
         private String secureId;
+        @Schema(description = "만료시간을 암호화하는 Key",
+                example = "987secure6543Key", type = "String")
         private String secretKey;
     }
 
@@ -28,9 +33,14 @@ public class QrCodeDto {
     public static class QrcodeRequestDto{
         @NotNull(message = "null은 허용하지 않습니다.")
         @NotBlank(message = "공백을 허용하지 않습니다.")
+        @Schema(description = "암호화된 Event PK",
+                example = "987secure6543code", type = "String")
         private String secureCode;
+
         @NotNull(message = "null은 허용하지 않습니다.")
         @NotBlank(message = "공백을 허용하지 않습니다.")
+        @Schema(description = "암호화된 만료시간",
+                example = "12expire345time", type = "String")
         private String secureExpireTime;
     }
 
@@ -38,9 +48,19 @@ public class QrCodeDto {
     @Builder
     @NoArgsConstructor @AllArgsConstructor
     public static class QrcodeResponseDto{
+        @Schema(description = "학생 이름",
+                example = "studentMember", type = "String")
         private String studentName;
+
+        @Schema(description = "학생 학번",
+                example = "20191511", type = "String")
         private String studentId;
+
+        @Schema(description = "이벤트 명",
+                example = "eventName", type = "String")
         private String eventName;
+
+        @Schema(description = "HATEOAS 링크 - ref와 href만 제공")
         private List<Link> link;
     }
 
