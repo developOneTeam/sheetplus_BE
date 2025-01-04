2897189bb4320dd9fd7fd9ca4e46c0294b5ebf86
hwangjeyeon <kingpele9018@gmail.com>
2025-01-04 12:47:37 +0900

    Docs: Student Page API 문서화 작업
    - URI 설명 구체화
    - DTO 설명 구체화

--- src/main/java/sheetplus/checkings/business/page/student/controller/StudentPageController.java
+++ src/main/java/sheetplus/checkings/business/page/student/controller/StudentPageController.java
@@ -1,29 +1,53 @@
 package sheetplus.checkings.business.page.student.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sheetplus.checkings.business.page.student.dto.StudentPageDto.ActivitiesResponseDto;
 import sheetplus.checkings.business.page.student.dto.StudentPageDto.StudentHomePageResponseDto;
 import sheetplus.checkings.business.page.student.dto.StudentPageDto.StudentPageActivitiesResponseDto;
-import sheetplus.checkings.domain.event.dto.EventDto.EventResponseDto;
 import sheetplus.checkings.business.page.student.service.StudentPageService;
-
-import java.util.List;
+import sheetplus.checkings.exception.error.ErrorResponse;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("private/student")
+@Tag(name = "Student-Page", description = "StudentPage Service API")
 public class StudentPageController {
 
     private final StudentPageService studentPageService;
 
-
-    @GetMapping("private/student/{contest}/home/v1")
+    @GetMapping("/contests/{contest}/home/v1")
+    @Operation(summary = "Student-Page Home GET", description = "학생 페이지 Home 화면 데이터를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "이벤트 즐겨찾기 조회 성공",
+                    content = @Content(schema =
+                    @Schema(implementation = StudentHomePageResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "잘못된 HTTP 입력 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Member]정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+    })
     public ResponseEntity<StudentHomePageResponseDto> readStudentHome(
+            @Parameter(description = "액세스 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "Authorization", required = false) String token,
+
+            @Parameter(description = "Contest PK", example = "1")
             @PathVariable("contest") Long contestId){
 
         token = token.replace("Bearer ", "");
@@ -31,27 +55,35 @@ public class StudentPageController {
         return ResponseEntity.ok(studentPageService.readStudentHomePage(token, contestId));
     }
 
-    @GetMapping("public/{contest}/schedule/v1")
-    public ResponseEntity<List<EventResponseDto>> readStudentSchedule(
-            @PathVariable("contest") Long contestId,
-            @RequestParam(value = "offset", required = false)
-            Integer offset,
-            @RequestParam(value = "limit", required = false)
-            Integer limit
-    ){
-        return ResponseEntity.ok(studentPageService
-                .readStudentSchedulePage(contestId, PageRequest.of(offset-1, limit)));
-    }
 
-    @GetMapping("private/student/{contest}/activities/v1")
+
+    @GetMapping("/contests/{contest}/activities/v1")
+    @Operation(summary = "Student-Page activities GET", description = "학생 페이지 Activity 화면 데이터를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "학생 페이지 Activity 화면 데이터 조회 성공",
+                    content = @Content(schema =
+                    @Schema(implementation = StudentHomePageResponseDto.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "잘못된 HTTP 입력 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "[Participate] 정보를 찾을 수 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+    })
     public ResponseEntity<StudentPageActivitiesResponseDto> readStudentActivities(
+            @Parameter(description = "액세스 토큰입니다 Header에 포함해서 요청하세요", hidden = true)
             @RequestHeader(value = "Authorization", required = false) String token,
+
+            @Parameter(description = "Contest PK", example = "1")
             @PathVariable("contest") Long contestId){
 
         token = token.replace("Bearer ", "");
         ActivitiesResponseDto activitiesResponseDto
                 = studentPageService.readStudentActivitiesPage(token, contestId);
-
 
         return ResponseEntity.ok(StudentPageActivitiesResponseDto
                 .builder()
