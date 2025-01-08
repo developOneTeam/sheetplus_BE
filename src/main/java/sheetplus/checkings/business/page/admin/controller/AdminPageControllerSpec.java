d9cfa80cfb30b9ae99a78ac47b7d26f76008151c
hwangjeyeon <kingpele9018@gmail.com>
2025-01-09 01:12:36 +0900

    Docs: Contest정보와 연관된 Event,Entry개수 파악하는 기능 API문서 작성

--- src/main/java/sheetplus/checkings/business/page/admin/controller/AdminPageControllerSpec.java
+++ src/main/java/sheetplus/checkings/business/page/admin/controller/AdminPageControllerSpec.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import sheetplus.checkings.business.page.admin.dto.AdminPageDto.AdminHomeResponseDto;
+import sheetplus.checkings.business.page.admin.dto.AdminPageDto.ContestInfoWithCounts;
 import sheetplus.checkings.domain.member.dto.MemberDto.MemberInfoResponseDto;
 import sheetplus.checkings.exception.error.ErrorResponse;
 
@@ -80,4 +81,26 @@ public interface AdminPageControllerSpec {
             Integer offset,
             @Parameter(description = "페이지당 조회할 데이터 개수", example = "1")
             Integer limit);
+
+
+    @Operation(summary = "Admin-Page ContestInfo with Counts", description = "Admin-Page Contest 정보 조회 화면")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contest 정보 조회를 성공했습니다.",
+                    content = @Content(array = @ArraySchema(schema =
+                    @Schema(implementation = MemberInfoResponseDto.class)),
+                            mediaType = "application/json"),
+                    headers = {@Header(name = "etag",
+                            description = "\"etagexample\"과 같은 형태로 제공됩니다. If-None-Match속성에 Etag를 추가해서 요청하세요"),
+                            @Header(name = "Cache-Control",
+                                    description = "클라이언트 캐시 사용, 캐싱 최대유효시간 1시간, 유효시간 지난 후에는 반드시 서버로 재요청하세요")}),
+            @ApiResponse(responseCode = "304", description = "캐시 데이터의 변경사항이 없습니다. 로컬 캐시 데이터를 사용하세요",
+                    content = @Content (mediaType = "None")),
+            @ApiResponse(responseCode = "401", description = "액세스 토큰이 없습니다",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "접근 권한이 없는 사용자입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = "application/json"))
+    })
+    ResponseEntity<List<ContestInfoWithCounts>> readContestInfos();
 }
