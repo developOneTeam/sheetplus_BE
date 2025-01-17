d97209f7c926cc46120ae21fe683ef281aaf67f1
hwangjeyeon <kingpele9018@gmail.com>
2025-01-17 23:02:08 +0900

    Delete: 적용되지 않은 검증 애노테이션 삭제

--- src/main/java/sheetplus/checkings/domain/participatecontest/dto/ParticipateContestDto.java
+++ src/main/java/sheetplus/checkings/domain/participatecontest/dto/ParticipateContestDto.java
@@ -1,6 +1,5 @@
 package sheetplus.checkings.domain.participatecontest.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,13 +13,8 @@ public class ParticipateContestDto {
     @Builder
     @NoArgsConstructor @AllArgsConstructor
     public static class ParticipateInfoResponseDto{
-        @NotNull(message = "응답 데이터 검증 오류: null은 허용하지 않습니다.")
-        private Long completeEventMemberCounts;
-        @NotNull(message = "응답 데이터 검증 오류: null은 허용하지 않습니다.")
         private Long moreThanOneCounts;
-        @NotNull(message = "응답 데이터 검증 오류: null은 허용하지 않습니다.")
         private Long moreThanFiveCounts;
-
     }
 
 }
