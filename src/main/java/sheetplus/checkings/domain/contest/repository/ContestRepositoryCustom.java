b257b49a2cc5420e87551373d3ecff6e528f441a
hwangjeyeon <kingpele9018@gmail.com>
2025-01-18 12:51:22 +0900

    Feat: EventStats, EntryStats 조회를 위한 쿼리 추가
    - ContestId만으로 조회, 페이징 추가, 프로젝션 사용

--- src/main/java/sheetplus/checkings/domain/contest/repository/ContestRepositoryCustom.java
+++ src/main/java/sheetplus/checkings/domain/contest/repository/ContestRepositoryCustom.java
@@ -1,7 +1,9 @@
 package sheetplus.checkings.domain.contest.repository;
 
 import org.springframework.data.domain.Pageable;
+import sheetplus.checkings.business.page.admin.dto.AdminPageDto.AdminEventStatsDto;
 import sheetplus.checkings.business.page.admin.dto.AdminPageDto.ContestInfoWithCounts;
+import sheetplus.checkings.domain.entry.dto.EntryDto.EntryExceptLinksResponseDto;
 import sheetplus.checkings.domain.event.dto.EventDto.EventResponseDto;
 import sheetplus.checkings.domain.enums.EventCategory;
 
@@ -13,5 +15,6 @@ public interface ContestRepositoryCustom {
     List<EventResponseDto> findTodayEvents(Long contestId, Pageable pageable);
     List<EventResponseDto> findParticipateEvents(Long contestId, List<EventCategory> category);
     List<ContestInfoWithCounts> findContestInfoWithCounts();
-
+    List<EntryExceptLinksResponseDto> findContestWithEntries(Long contestId, Pageable pageable);
+    AdminEventStatsDto findContestWithEvents(Long contestId, Pageable pageable);
 }
--- src/main/java/sheetplus/checkings/domain/contest/repository/ContestRepositoryCustomImpl.java
+++ src/main/java/sheetplus/checkings/domain/contest/repository/ContestRepositoryCustomImpl.java
@@ -1,10 +1,17 @@
 package sheetplus.checkings.domain.contest.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import sheetplus.checkings.business.page.admin.dto.AdminPageDto.AdminEventStatsDto;
 import sheetplus.checkings.business.page.admin.dto.AdminPageDto.ContestInfoWithCounts;
+import sheetplus.checkings.domain.entry.dto.EntryDto.EntryExceptLinksResponseDto;
+import sheetplus.checkings.domain.enums.ContestCons;
+import sheetplus.checkings.domain.enums.EntryType;
+import sheetplus.checkings.domain.enums.EventType;
+import sheetplus.checkings.domain.event.dto.EventDto.EventExceptLinksResponseDto;
 import sheetplus.checkings.domain.event.dto.EventDto.EventResponseDto;
 import sheetplus.checkings.domain.contest.entity.Contest;
 import sheetplus.checkings.domain.event.entity.Event;
@@ -16,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static sheetplus.checkings.domain.contest.entity.QContest.contest;
+import static sheetplus.checkings.domain.entry.entity.QEntry.entry;
 import static sheetplus.checkings.domain.event.entity.QEvent.event;
 import static sheetplus.checkings.domain.participatecontest.entity.QParticipateContest.participateContest;
 import static sheetplus.checkings.exception.error.ApiError.CONTEST_NOT_FOUND;
@@ -125,5 +133,92 @@ public class ContestRepositoryCustomImpl implements ContestRepositoryCustom{
                         .entryCounts(p.getEntries().size())
                         .build())
                 .toList();
+    }
+
+    @Override
+    public List<EntryExceptLinksResponseDto> findContestWithEntries(Long contestId, Pageable pageable) {
+        return queryFactory
+                .select(Projections.constructor(
+                        EntryExceptLinksResponseDto.class,
+                        entry.id.as("id"),
+                        entry.name.as("name"),
+                        entry.location.as("location"),
+                        entry.building.as("building"),
+                        entry.teamNumber.as("teamNumber"),
+                        entry.major.as("major"),
+                        entry.professorName.as("professorName"),
+                        entry.leaderName.as("leaderName"),
+                        entry.entryType
+                                .when(EntryType.PRELIMINARY).then(EntryType.FINALS.getMessage())
+                                .when(EntryType.FINALS).then(EntryType.PRELIMINARY.getMessage())
+                                .otherwise("Unknown Type")
+                                .as("entryType")
+                ))
+                .from(contest)
+                .innerJoin(entry)
+                .on(entry.entryContest.id.eq(contestId))
+                .where(contest.id.eq(contestId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public AdminEventStatsDto findContestWithEvents(Long contestId, Pageable pageable) {
+        List<EventExceptLinksResponseDto> events = queryFactory.select(
+                Projections.constructor(
+                                EventExceptLinksResponseDto.class,
+                                event.id,
+                                event.name,
+                                event.startTime,
+                                event.endTime,
+                                event.location,
+                                event.building,
+                                event.speakerName,
+                                event.major,
+                                event.eventCondition
+                                        .when(ContestCons.EVENT_PROGRESS)
+                                        .then(ContestCons.EVENT_PROGRESS.getMessage())
+                                        .when(ContestCons.EVENT_BEFORE)
+                                        .then(ContestCons.EVENT_BEFORE.getMessage())
+                                        .when(ContestCons.EVENT_FINISH)
+                                        .then(ContestCons.EVENT_FINISH.getMessage())
+                                        .otherwise("Unknown Condition")
+                                        .as("conditionMessage"),
+                                event.eventType
+                                        .when(EventType.NO_CHECKING)
+                                        .then(EventType.NO_CHECKING.getMessage())
+                                        .when(EventType.CHECKING)
+                                        .then(EventType.CHECKING.getMessage())
+                                        .otherwise("Unknown Type")
+                                        .as("eventTypeMessage"),
+                                event.eventCategory
+                                        .when(EventCategory.EVENT_ONE)
+                                        .then(EventCategory.EVENT_ONE.getMessage())
+                                        .when(EventCategory.EVENT_TWO)
+                                        .then(EventCategory.EVENT_TWO.getMessage())
+                                        .when(EventCategory.EVENT_THREE)
+                                        .then(EventCategory.EVENT_THREE.getMessage())
+                                        .when(EventCategory.EVENT_FOUR)
+                                        .then(EventCategory.EVENT_FOUR.getMessage())
+                                        .when(EventCategory.EVENT_FIVE)
+                                        .then(EventCategory.EVENT_FIVE.getMessage())
+                                        .otherwise("unknown Category")
+                                        .as("categoryMessage")
+                        )
+                )
+                .from(contest)
+                .innerJoin(event)
+                .on(event.eventContest.id.eq(contestId))
+                .where(contest.id.eq(contestId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return AdminEventStatsDto
+                .builder()
+                .eventCounts(events.size())
+                .allEvents(events)
+                .build();
     }
 }
