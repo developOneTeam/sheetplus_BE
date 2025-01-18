d98b012810f174dc98e7c4ec249d51a8ae688a39
hwangjeyeon <kingpele9018@gmail.com>
2025-01-18 21:04:08 +0900

    Refactor: CommonPage, StudentPage 조회로직 최적화
    - QueryDsl 활용

--- src/main/java/sheetplus/checkings/domain/contest/repository/ContestRepositoryCustom.java
+++ src/main/java/sheetplus/checkings/domain/contest/repository/ContestRepositoryCustom.java
@@ -4,19 +4,20 @@ import org.springframework.data.domain.Pageable;
 import sheetplus.checkings.business.page.admin.dto.AdminPageDto.AdminContestStatsDto;
 import sheetplus.checkings.business.page.admin.dto.AdminPageDto.AdminEventStatsDto;
 import sheetplus.checkings.business.page.admin.dto.AdminPageDto.ContestInfoWithCounts;
+import sheetplus.checkings.domain.contest.dto.ContestDto.ContestInfoResponseDto;
 import sheetplus.checkings.domain.entry.dto.EntryDto.EntryExceptLinksResponseDto;
-import sheetplus.checkings.domain.event.dto.EventDto.EventResponseDto;
-import sheetplus.checkings.domain.enums.EventCategory;
+import sheetplus.checkings.domain.event.dto.EventDto.EventExceptLinksResponseDto;
 
 import java.util.List;
 
 public interface ContestRepositoryCustom {
 
-    List<EventResponseDto> findNowAfterEvents(Long contestId);
-    List<EventResponseDto> findTodayEvents(Long contestId, Pageable pageable);
-    List<EventResponseDto> findParticipateEvents(Long contestId, List<EventCategory> category);
+    List<EventExceptLinksResponseDto> findNowAfterEvents(Long contestId);
+    List<EventExceptLinksResponseDto> findTodayEvents(Long contestId, Pageable pageable);
+    List<EventExceptLinksResponseDto> findParticipateEvents(Long ParticipateId);
     List<ContestInfoWithCounts> findContestInfoWithCounts();
     List<EntryExceptLinksResponseDto> findContestWithEntries(Long contestId, Pageable pageable);
     AdminEventStatsDto findContestWithEvents(Long contestId, Pageable pageable);
     AdminContestStatsDto findContestStats(Long contestId);
+    List<ContestInfoResponseDto> findAllContestInfo(Pageable pageable);
 }
