6f9c38be654d684cca4a1caedcdbb9c91f9f67ab
hwangjeyeon <kingpele9018@gmail.com>
2025-01-21 18:53:11 +0900

    Delete: 쿼리 조회방식 통합에 따른 불필요한 DTO 삭제

--- src/main/java/sheetplus/checkings/business/page/admin/dto/AdminPageDto.java
+++ src/main/java/sheetplus/checkings/business/page/admin/dto/AdminPageDto.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sheetplus.checkings.domain.entry.dto.EntryDto.EntryExceptLinksResponseDto;
 import sheetplus.checkings.domain.entry.dto.EntryDto.EntryResponseDto;
-import sheetplus.checkings.domain.enums.ContestCons;
 import sheetplus.checkings.domain.event.dto.EventDto.EventExceptLinksResponseDto;
 import sheetplus.checkings.domain.participatecontest.dto.ParticipateContestDto.ParticipateInfoResponseDto;
 
@@ -105,70 +104,6 @@ public class AdminPageDto {
                 example = "20", type = "String")
         private Long notTodayEvents;
         // 우상 3
-        @Schema(description = "출품작 학과 수",
-                example = "7", type = "String")
-        private Long entryMajorCounts;
-        @Schema(description = "출품작 수",
-                example = "1500", type = "String")
-        private Long entryCounts;
-        @Schema(description = "예선 출품작",
-                example = "1400", type = "String")
-        private Long entryPreliminaryCounts;
-        @Schema(description = "본선 출품작",
-                example = "100", type = "String")
-        private Long entryFinalCounts;
-    }
-
-    @Getter
-    @Builder
-    @NoArgsConstructor @AllArgsConstructor
-    @Schema(description = "어드민 페이지 대회정보 통계 - Contest정보", contentMediaType = "application/json")
-    public static class AdminContestStatsAboutContestDto{
-        // 우상 제목
-        @Schema(description = "Contest 이름",
-                example = "contest", type = "String")
-        private String contestName;
-
-        @Schema(description = "Contest 시작시간",
-                example = "2025-01-04 12:09:01", type = "string", pattern = "yyyy-MM-dd HH:mm:ss")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-        private LocalDateTime contestStart;
-
-        @Schema(description = "Contest 종료시간",
-                example = "2025-01-04 12:09:01", type = "string", pattern = "yyyy-MM-dd HH:mm:ss")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-        private LocalDateTime contestEnd;
-    }
-
-    @Getter
-    @Builder
-    @NoArgsConstructor @AllArgsConstructor
-    @Schema(description = "어드민 페이지 대회정보 통계 - Event통계", contentMediaType = "application/json")
-    public static class AdminContestStatsAboutEventDto{
-        @Schema(description = "이벤트 장소명",
-                example = "location", type = "String")
-        private String locationName;
-
-        @Schema(description = "이벤트 장소 수",
-                example = "100", type = "String")
-        private Long locationCounts;
-        // 우상 3
-        @Schema(description = "남은 이벤트 수",
-                example = "30", type = "String")
-        private Long remainEvents;
-        @Schema(description = "종료한 이벤트 수",
-                example = "50", type = "String")
-        private Long finishEvents;
-        @Schema(description = "오늘 시작하지 않는 이벤트 수",
-                example = "20", type = "String")
-        private Long notTodayEvents;
-    }
-
-    @Getter
-    @Builder
-    @NoArgsConstructor @AllArgsConstructor
-    @Schema(description = "어드민 페이지 대회정보 통계 - Entry통계", contentMediaType = "application/json")
-    public static class AdminContestStatsAboutEntryDto{
         @Schema(description = "출품작 학과 수",
                 example = "7", type = "String")
         private Long entryMajorCounts;
